93b6bee5753a65fe9af08f5fab4f76b2755981f2
Nataliia <dominok56@gmail.com>
2020-10-11 19:20:15 +0300

    first commit

---
+++ Task6HW/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task6HW
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string f = textBox1.Text;
+                string n = textBox2.Text;
+                string d = textBox3.Text;
+                double a = double.Parse(f);
+                double b = double.Parse(n);
+                double c = double.Parse(d);
+                if (c > 180 || c < 0)
+                {
+                    MessageBox.Show("Something is incorrect");
+                }
+                else
+                {
+                    Triangle triangle = new Triangle(a, b, c);
+                    if (triangle.IsEquality())
+                    {
+                        EqulibriumTriangle Etriangle = new EqulibriumTriangle();
+                        double per = triangle.Perimeter();
+                        double area = triangle.Area();
+                        MessageBox.Show("Is equality  triangle" + "\n" + "Perimeter - " + per + "\n" + "Area - " + area);
+                    }
+                    else if (triangle.IsIsosceles())
+                    {
+                        IsoscelesTriangle Itriangle = new IsoscelesTriangle();
+                        double per = triangle.Perimeter();
+                        double area = triangle.Area();
+                        MessageBox.Show("Is isosceles triangle" + "\n" + "Perimeter - " + per + "\n" + "Area - " + area);
+                    }
+                    else
+                    {
+                        double per = triangle.Perimeter();
+                        double area = triangle.Area();
+                        MessageBox.Show("Perimeter - " + per + "\n" + "Area - " + area);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
---
+++ Task6HW/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task6HW
+{
+    public class Triangle
+    {
+        public double FirstSide, SecondSide, Angle;
+
+        public Triangle() { }
+        public Triangle(double a, double b, double c)
+        {
+            FirstSide = a;
+            SecondSide = b;
+            Angle = c;
+        }
+        public bool IsEquality()
+        {
+            return FirstSide == SecondSide && Angle == 60;
+        }
+        public bool IsIsosceles()
+        {
+            return FirstSide == SecondSide && Angle != 60;
+        }
+        public double GetThirdSide()
+        {
+            double ThirdSide = Math.Sqrt(Math.Pow(FirstSide, 2) + Math.Pow(SecondSide, 2) - 2 * FirstSide * SecondSide * Math.Cos(Angle));
+            return ThirdSide;
+        }
+        virtual public double Perimeter()
+        {
+            return FirstSide + SecondSide + GetThirdSide();
+        }
+        virtual public double Area()
+        {
+            double p = 0.5 * Perimeter();
+            return Math.Sqrt(p * (p - FirstSide) * (p - SecondSide) * (p - GetThirdSide()));
+        }
+    }
+
+    public class EqulibriumTriangle : Triangle
+    {
+        virtual public double EqulibriumTrianglePerimeter()
+        {
+            return FirstSide * 3;
+        }
+        virtual public double EqulibriumTriangleArea()
+        {
+            return (FirstSide * FirstSide * Math.Sqrt(3)) / 4;
+        }
+    }
+    public class IsoscelesTriangle : Triangle
+    {
+        virtual public double IsoscelesTrianglePerimeter()
+        {
+            return FirstSide * 2 + GetThirdSide();
+        }
+        virtual public double IsoscelesTriangleArea()
+        {
+            return (1 / 2) * GetThirdSide() * Math.Sqrt(FirstSide * FirstSide - (1 / 4) * SecondSide * SecondSide);
+        }
+    }
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
